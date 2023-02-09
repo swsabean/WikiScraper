82cecb6d408014820038fbf803e2a261f2efe0c3
swsabean <swsabean@yahoo.com>
2023-02-09 01:52:46 -0600

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,3 +1,4 @@
+
 import requests
 from bs4 import BeautifulSoup
 
@@ -16,7 +17,14 @@ def scrape_wikipedia(url):
         # Extract the text from the main content
         text = main_content.get_text()
 
-        return text
+        # Find the first section header and its content
+        first_header = main_content.find('h2')
+        first_section = first_header.find_next_sibling('p')
+
+        # Extract the text from the first section
+        first_section_text = first_section.get_text()
+
+        return text, first_header.get_text(), first_section_text
     else:
         # Return an error message if the request was not successful
         return f'Failed to scrape {url}. Error code: {response.status_code}'
@@ -25,11 +33,13 @@ if __name__ == '__main__':
     # Prompt the user for a Wikipedia page title
     page_title = input('Enter the title of a Wikipedia page: ')
 
-   # Build the URL for the Wikipedia article
+    # Build the URL for the Wikipedia article
     url = f'https://en.wikipedia.org/wiki/{page_title}'
 
     # Scrape the text from the Wikipedia article
-    text = scrape_wikipedia(url)
+    text, first_header, first_section = scrape_wikipedia(url)
 
-    # Print the extracted text
-    print(text)
+    # Print the extracted text, first section header, and first section content
+    print(f'Text: {text}')
+    print(f'First header: {first_header}')
+    print(f'First section: {first_section}')
